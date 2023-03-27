269ec2e04740cc5bfdcfb333b41ac52117163e70
Kyle Kaczorowski <kmk4427@rit.edu>
2023-03-26 23:03:36 -0400

    login made, should be sql injectable

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, request, flash, g
+from .models import User
+from sqlalchemy import Engine,text
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -7,9 +10,17 @@ auth = Blueprint('auth', __name__)
 def login():
     # data = request.form # Gets data from the form that did the request
     # if request.method == 'POST' ~~~~
+    #flash("TESTT", 'error')
     ## XXX = request.form.get('email')
     if(request.method == 'POST'):
-        flash("TESTT", 'error')
+        username = request.form.get('name')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            rs = db.session.execute(text("SELECT * FROM user WHERE username='" + username +"' AND password='+password'"))
+            print('executing: ')
+            print(rs)
     return render_template("login.html", text="Testing")
 
 @auth.route('/logout')
